c6d66b0436877bfb703c3d1e7a097129c6f9d0c9
Jonnier197 <Jonnier.cadena01@unicatolica.edu.co>
2025-05-30 22:09:19 -0500

    feat: agregar implementación inicial de MinHeap

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
- 
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, name, priority):
+        self.heap.append((priority, name))
+        self._heapify_up(len(self.heap) - 1)
+
+    def extract_min(self):
+        if not self.heap:
+            return None
+        self._swap(0, len(self.heap) - 1)
+        min_task = self.heap.pop()
+        self._heapify_down(0)
+        return min_task
+
+    def _heapify_up(self, index):
+        parent = (index - 1) // 2
+        while index > 0 and self.heap[index][0] < self.heap[parent][0]:
+            self._swap(index, parent)
+            index = parent
+            parent = (index - 1) // 2
+
+    def _heapify_down(self, index):
+        smallest = index
+        left = 2 * index + 1
+        right = 2 * index + 2
+
+        if left < len(self.heap) and self.heap[left][0] < self.heap[smallest][0]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right][0] < self.heap[smallest][0]:
+            smallest = right
+
+        if smallest != index:
+            self._swap(index, smallest)
+            self._heapify_down(smallest)
+
+    def _swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+
+    def print_heap(self):
+        print("Estado actual del heap:")
+        for priority, name in self.heap:
+            print(f"Tarea: {name}, Prioridad: {priority}")
